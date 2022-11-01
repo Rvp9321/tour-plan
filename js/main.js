a14f1115b0c2b46969a77eb16083997dc2c52023
rvp9321 <p.roman@list.ru>
2022-11-01 11:14:21 +0400

    fix: added ESC option to close modal

--- js/main.js
+++ js/main.js
@@ -42,6 +42,17 @@ $(document).ready(function () {
   var closeModalButton = $(".modal__close")
   modalButton.on('click', openModal);
   closeModalButton.on('click', closeModal);
+
+  document.body.addEventListener('keyup', function (e) {
+    var key = e.keyCode;
+
+    if (key == 27) {
+      var modalOverlay = $(".modal__overlay");
+      var modalDialog = $(".modal__dialog");
+      modalOverlay.removeClass('modal__overlay--visible');
+      modalDialog.removeClass('modal__dialog--visible');
+    };
+  }, false);
     
   function openModal() {
     var targetModal = $(this).attr("data-href"); // взять у этого (this) элемента аттрибут data-href 
